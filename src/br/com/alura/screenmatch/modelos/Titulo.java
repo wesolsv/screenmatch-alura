659284bc1e5f897d1e3cef15f3bf73213548ccad
Wesley Oliveira Silva <wesolsv@gmail.com>
2023-04-26 09:51:01 -0300

    ajustes

--- src/br/com/alura/screenmatch/modelos/Titulo.java
+++ src/br/com/alura/screenmatch/modelos/Titulo.java
@@ -1,7 +1,6 @@
 package br.com.alura.screenmatch.modelos;
 
 import br.com.alura.screenmatch.excecao.ErroAnoException;
-import com.google.gson.annotations.SerializedName;
 
 public class Titulo implements Comparable<Titulo> {
     private String nome;
